dcb1cd9f87169a17a2583f3283a28996cd6c64cf
Andrew <wwrdna@gmail.com>
2016-06-22 19:15:13 +0500

    Folder name change

--- DemoLibrary/Properties/AssemblyInfo.cs
+++ DemoLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("DemoLibrary")]
+[assembly: AssemblyTitle("MjpegLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DemoLibrary")]
+[assembly: AssemblyProduct("MjpegLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
