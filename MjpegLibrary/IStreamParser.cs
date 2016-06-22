b83b554832fe034b65c2f74093ae07b0189d6fe9
Andrew <wwrdna@gmail.com>
2016-06-22 19:23:42 +0500

    Renaming folder again

---
+++ MjpegLibrary/IStreamParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace MjpegLibrary
+{
+    interface IStreamParser
+    {
+        event Action InvalidStream;
+        event Action<byte[]> PacketFound;
+
+        bool IsParsing { get; }
+
+        void StartParsing(Stream stream);
+        void StopParsing();
+    }
+}
---
+++ MjpegLibrary/IStreamSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MjpegLibrary
+{
+    public interface IStreamSource
+    {
+        Stream GetStream();
+        Task<Stream> GetStreamAsync();
+    }
+}
