6ce1cdcda1599284dffdcb57309c6f5ad4f58200
Andrew <wwrdna@gmail.com>
2016-06-24 20:11:08 +0500

    Moved initialization of some variables from method to constructor

--- MjpegLibrary/MjpegStreamParser.cs
+++ MjpegLibrary/MjpegStreamParser.cs
@@ -10,6 +10,8 @@ namespace MjpegLibrary
         public event Action InvalidStream = () => { };
         public event Action<byte[]> PacketFound = (bytes) => { };
 
+        private readonly byte[] delimiterBytes;
+        private readonly byte[] buffer;
         private readonly int bufferSize;
         private readonly string packetDelimiter;
         private bool shouldStop;
@@ -17,7 +19,9 @@ namespace MjpegLibrary
         public MjpegStreamParser(string packetDelimiter)
         {
             this.packetDelimiter = packetDelimiter;
+            this.delimiterBytes = Encoding.UTF8.GetBytes(packetDelimiter); // Could get encoding from webResponse
             this.bufferSize = 4096;
+            this.buffer = new byte[bufferSize];
         }
 
         public bool IsParsing { get; private set; }
@@ -26,11 +30,9 @@ namespace MjpegLibrary
         {
             // In this algorithm I assume that buffer always have either 1 or 0 packet delimiters in it,
             // and delimiter will always be at start of buffer, since I've seen only such packets.
-            shouldStop = false;
-            IsParsing = true;
+            this.shouldStop = false;
+            this.IsParsing = true;
             int bytesRead = 0;
-            byte[] boundary = Encoding.UTF8.GetBytes(packetDelimiter); // Could get encoding from webResponse
-            byte[] buffer = new byte[bufferSize];
             var packetBytes = new List<byte>();
 
             while (!shouldStop)
@@ -43,12 +45,10 @@ namespace MjpegLibrary
                 {
                     break;
                 }
-                if (bytesRead == -1) // end of stream
-                {
+                if (bytesRead == -1) // end of stream                
                     break;
-                }
-                
-                int boundaryStartsAt = FindStartOfSubArray(buffer, boundary);
+
+                int boundaryStartsAt = FindStartOfSubArray(buffer, delimiterBytes);
                 if (boundaryStartsAt == 0 && packetBytes.Count != 0) // start of new packet found in buffer
                 {
                     PacketFound(packetBytes.ToArray());
@@ -87,9 +87,8 @@ namespace MjpegLibrary
                         break;
                 }
                 if (i == subArray.Length)
-                {
                     return startOfSubArray;
-                }                
+
                 searchStartIndex = startOfSubArray + 1;
                 leftToCheck = array.Length - searchStartIndex;
             }
